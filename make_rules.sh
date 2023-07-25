99674ccec4fd2f7ef3eca2df35c8cc30b64ea2c7
Atsuya <02milkey15@gmail.com>
2023-07-26 06:07:43 +0900

    FizzBuzz

--- make_rules.sh
+++ make_rules.sh
@@ -1,6 +1,29 @@
 #!/bin/bash -x
 
-iptables -F OUTPUT
-iptables -A OUTPUT -s 192.168.0.103 -d 192.168.0.103 -p icmp --icmp-type 8 -m length --length 38 -j REJECT --reject-with icmp-host-unreachable
+iptables -F
+
+iptables -N COUNTER_CHAIN
+iptables -N LOGIC_CHAIN
+iptables -N NOTHING
+iptables -N FIZZ
+iptables -N BUZZ
+iptables -N FIZZBUZZ
+
+iptables -A OUTPUT -p icmp --icmp-type 8 -j COUNTER_CHAIN
+iptables -A OUTPUT -p icmp --icmp-type 8 -j LOGIC_CHAIN
+
+iptables -A COUNTER_CHAIN -m recent --name fizz     --set
+iptables -A COUNTER_CHAIN -m recent --name buzz     --set
+iptables -A COUNTER_CHAIN -m recent --name fizzbuzz --set
+
+iptables -A LOGIC_CHAIN -m recent --name fizzbuzz --rcheck --hitcount 15 -j FIZZBUZZ
+iptables -A LOGIC_CHAIN -m recent --name buzz     --rcheck --hitcount 5  -j BUZZ
+iptables -A LOGIC_CHAIN -m recent --name fizz     --rcheck --hitcount 3  -j FIZZ
+iptables -A LOGIC_CHAIN                                                  -j NOTHING
+
+iptables -A FIZZBUZZ -m recent --name fizzbuzz --remove -j REJECT --reject-with icmp-port-unreachable  # CODE = 3
+iptables -A BUZZ     -m recent --name buzz     --remove -j REJECT --reject-with icmp-proto-unreachable # CODE = 2
+iptables -A FIZZ     -m recent --name fizz     --remove -j REJECT --reject-with icmp-host-unreachable  # CODE = 1
+iptables -A NOTHING                                     -j REJECT --reject-with icmp-net-unreachable   # CODE = 0
 
 iptables-save | iptables-restore
